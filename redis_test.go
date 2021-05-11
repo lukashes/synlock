86ade7cbce6e218dc33fe118b03245e26e828882
Yegor Lukash <yegor.lukash@gmail.com>
2021-05-11 18:18:06 +0300

    Test for context redis mutex

--- redis_test.go
+++ redis_test.go
@@ -61,3 +61,47 @@ func TestRedisMutex(t *testing.T) {
 
 	wg.Wait()
 }
+
+func TestRedisMutexContextCancellation(t *testing.T) {
+	r, err := NewRedis(DefRedisOpts)
+	if err != nil {
+		t.Fatalf("init redis object error: %s", err)
+	}
+
+	m1, err := r.NewMutex(123)
+	if err != nil {
+		t.Fatalf("making new mutex error: %s", err)
+	}
+	mu1 := MustRedisMutex(m1)
+
+	m2, err := r.NewMutex(123)
+	if err != nil {
+		t.Fatalf("making new mutex error: %s", err)
+	}
+	mu2 := MustRedisMutex(m2)
+
+	err = mu1.LockContext(context.Background())
+	if err != nil {
+		t.Fatalf("acquiring lock error: %s", err)
+	}
+	defer mu1.Unlock()
+
+	wait := make(chan struct{})
+	go func() {
+		defer close(wait)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*100)
+		defer cancel()
+
+		err = mu2.LockContext(ctx)
+		if err != ctx.Err() {
+			t.Fatalf("unexpected aqcuiring context lock error: %s", err)
+		}
+	}()
+
+	select {
+	case <-wait:
+	case <-time.After(time.Second):
+		t.Fatalf("Checking blocking context timeout")
+	}
+}
